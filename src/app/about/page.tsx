806aa4c804ac12f6db156fce2e07d2ec8f693e9b
SebastianL2 <sebas16cely@gmail.com>
2024-01-16 18:29:52 -0500

    def

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,169 +1,161 @@
 'use client'
 import React from "react";
 import Image from "next/image";
-import { Button, Typography } from "@material-tailwind/react";
 import { Navbar2 } from "@/components/navbar2";
 
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 
+import { useState, useEffect } from 'react'
+  interface DatosUsuario {
+    codigo: string;
+    DAN: string;
+    cedula: string;
+    clase: string;
+    createdAt: string;
+    email: string;
+    estadoSolicitud: string;
+    fechaActualizacion: string;
+    fechaExpedicion: string;
+    licencia: string;
+    nombre: string;
+    password: string;
+    role: string;
+    solicita: string;
+    updatedAt: string;
+    vigencia: string;
+  }
   function About() {
-   
+    const [data, setData] = useState<Array<DatosUsuario> | null>(null);
+    const [isLoading, setLoading] = useState(true)
+    const [message, setMessage] = useState('');
+    const handleUsuario = async () => {
+      try {
+        const efec =process.env.NEXT_PUBLIC_API_URL;
+        const userData = {
+          codigo: "123",
+          DAN: "456",
+          cedula: "789",
+          // ... otras propiedades ...
+          email: "usuario@example.com",
+          password: "contraseñaSegura",
+          // ... otras propiedades ...
+        };
+        
+        try {
+          const response = await fetch(`${efec}trainer/`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData),
+          });
+        
+          const data = await response.json();
+          console.log(data);
+        
+          if (response.ok) {
+            // Success status code, navigate to the desired page
+            // Por ejemplo, podrías redirigir al usuario a una página de éxito
+            // usando react-router o el enrutamiento de tu elección.
+          } else {
+            // Handle other status codes, e.g., display an error message
+            console.error('API request failed with status:', response.status);
+            const errorMessage = data.message;
+            // Aquí puedes manejar el mensaje de error como desees, por ejemplo,
+            // almacenarlo en un estado o mostrarlo al usuario.
+            setMessage(errorMessage);
+          }
+        } catch (error) {
+          console.error('Error during API request:', error);
+        }
+        
+    };
 
   
     return (
       <>
-      <Navbar2 scrolling={true}/>
-      <section className="py-12 px-8">
-      <div className="mx-auto max-w-screen-md">
-        <Typography placeholder={undefined} color="blue" variant="h6">
-          #science #biology #ecology
-        </Typography>
-        <Typography  placeholder={undefined} className="my-12 font-normal !text-gray-500">
-          Biology, often referred to as the &quot;science of life,&quot; is a
-          captivating and multifaceted field that delves into the intricate web
-          of living organisms and the systems that sustain them. From the
-          tiniest microorganisms to the towering giants of the animal kingdom,
-          biology offers a profound understanding of life&apos;s mechanisms,
-          evolution, and interconnectedness.
-        </Typography>
-
-        <Typography   placeholder={undefined} variant="h2" color="blue-gray" className="mt-8 mb-6">
-          Exploring the Wonders of Biology
-        </Typography>
-
-        <Typography  placeholder={undefined} className="my-10 font-normal !text-gray-500">
-          The Branches of Biology Biology encompasses a diverse array of
-          branches, each focusing on specific aspects of living organisms. Here
-          are a few key ones: a. Genetics: Genetics unravels the secrets of
-          heredity, studying how traits are passed from one generation to the
-          next and how variations occur. Recent advancements in genomics have
-          revolutionized medicine, enabling personalized treatments based on an
-          individual&apos;s genetic makeup. b. Ecology: Ecology delves into the
-          relationships between organisms and their environments. It plays a
-          pivotal role in understanding and addressing environmental challenges,
-          such as climate change and biodiversity loss.
-        </Typography>
-        <Image
-          width={768}
-          height={500}
-          src="/image/event.jpeg"
-          alt="post"
-          className="mb-4 h-[28rem] w-full rounded-xl object-cover"
+      <div className=" mr-60" >
+        <Card color="transparent" shadow={false}>
+      <Typography variant="h4" color="blue-gray">
+        Sign Up
+      </Typography>
+      <Typography color="gray" className="mt-1 font-normal">
+        Nice to meet you! Enter your details to register.
+      </Typography>
+      <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <div className="mb-1 flex flex-col gap-6">
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Name
+          </Typography>
+          <Input
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Your Email
+          </Typography>
+          <Input
+             
+            size="lg"
+            placeholder="name@mail.com"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+          <Typography variant="h6" color="blue-gray" className="-mb-3">
+            Password
+          </Typography>
+          <Input
+            type="password"
+            size="lg"
+            placeholder="********"
+            className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+            labelProps={{
+              className: "before:content-none after:content-none",
+            }}
+          />
+        </div>
+        <Checkbox
+          label={
+            <Typography
+              variant="small"
+              color="gray"
+              className="flex items-center font-normal"
+            >
+              I agree the
+              <a
+                href="#"
+                className="font-medium transition-colors hover:text-gray-900"
+              >
+                &nbsp;Terms and Conditions
+              </a>
+            </Typography>
+          }
+          containerProps={{ className: "-ml-2.5" }}
         />
-        <Typography  placeholder={undefined} variant="small" className="font-normal !text-gray-500">
-          Photo by Jackson Sophat on{" "}
-          <a
-            href="#"
-            target="_blank"
-            rel="noreferrer"
-            className="underline underline-offset-4 transition-colors hover:text-gray-900"
-          >
-            Unsplash
+        <Button className="mt-6" fullWidth onClick={handel}>
+          sign up
+        </Button>
+        <Typography color="gray" className="mt-4 text-center font-normal">
+          Already have an account?{" "}
+          <a href="#" className="font-medium text-gray-900">
+            Sign In
           </a>
         </Typography>
-
-        <Typography  placeholder={undefined} className="my-12 font-normal !text-gray-500">
-          Thank you Anna for the invite thank you to the whole Vogue team And I
-          love you like Kanye loves Kanye.
-          <br />
-          <br />
-          The Arctic Ocean freezes every winter and much of the sea-ice then
-          thaws every summer, and that process will continue whatever happens
-          with climate change. Even if the Arctic continues to be one of the
-          fastest-warming regions of the world, it will always be plunged into
-          bitterly cold polar dark every winter. And year-by-year, for all kinds
-          of natural reasons, there&apos;s huge variety of the state of the ice.
-        </Typography>
-
-        <Typography  placeholder={undefined}  variant="h2" color="blue-gray" className="mt-8 mb-6">
-          So what does the new record for the lowest level of winter ice
-          actually mean
-        </Typography>
-
-        <Typography  placeholder={undefined} className="my-10 font-normal !text-gray-500">
-          For a start, it does not automatically follow that a record amount of
-          ice will melt this summer. More important for determining the size of
-          the annual thaw is the state of the weather as the midnight sun
-          approaches and temperatures rise. But over the more than 30 years of
-          satellite records, scientists have observed a clear pattern of
-          decline, decade-by-decade.
-          <br />
-          <br />
-          The Arctic Ocean freezes every winter and much of the sea-ice then
-          thaws every summer, and that process will continue whatever happens
-          with climate change. Even if the Arctic continues to be one of the
-          fastest-warming regions of the world, it will always be plunged into
-          bitterly cold polar dark every winter. And year-by-year, for all kinds
-          of natural reasons, there&apos;s huge variety of the state of the ice.
-          <br />
-          <br />
-          Society has put up so many boundaries, so many limitations on
-          what&apos;s right and wrong that it&apos;s almost impossible to get a
-          pure thought out.
-        </Typography>
-        <div className="container mx-auto px-4 py-20">
-          <div className="w-full mb-10 md:flex items-center justify-between">
-            <div className="flex mb-5 md:mb-0 items-center gap-3">
-              <Button placeholder={undefined} size="sm" color="gray" variant="outlined">
-                stories
-              </Button>
-              <Button placeholder={undefined} size="sm" color="gray" variant="outlined">
-                design
-              </Button>
-              <Button  placeholder={undefined} size="sm" color="gray" variant="outlined">
-                catle
-              </Button>
-            </div>
-            <div className="flex items-center gap-3">
-              <button className="w-9 h-9 bg-[#35518D] hover:cursor-pointer rounded-lg flex items-center justify-center ">
-                <i className="text-white fab fa-facebook text-lg" />
-              </button>
-              <button className="w-9 h-9 bg-[#EA4C89] hover:cursor-pointer rounded-lg flex items-center justify-center ">
-                <i className="text-white fab fa-dribbble text-lg" />
-              </button>
-              <button className="w-9 h-9 bg-gray-900 hover:cursor-pointer rounded-lg flex items-center justify-center ">
-                <i className="text-white fab fa-github text-lg" />
-              </button>
-            </div>
-          </div>
-          <div className=" md:flex items-center justify-between">
-            <div className="mb-4 md:mb-0 md:flex items-center gap-5 max-w-2xl">
-              <div className="h-full mb-3 md:mb-0 w-full max-h-[4rem] max-w-[4rem] md:max-w-[6rem] md:max-h-[6rem] rounded-lg ">
-                <Image
-                  width={768}
-                  height={768}
-                  src="/image/avatar1.jpg"
-                  className="rounded-2xl"
-                  alt="photo"
-                />
-              </div>
-              <div>
-                <Typography
-                  placeholder={undefined}
-                  variant="h5"
-                  className="mb-4 md:mb-0"
-                  color="blue-gray"
-                >
-                  Emma Roberts
-                </Typography>
-                <Typography  placeholder={undefined} className="w-full md:w-10/12 font-normal !text-gray-500">
-                  Dr. Sarah Mitchell is a passionate and accomplished biologist
-                  with a deep-rooted fascination for the intricate workings of
-                  the natural world.
-                </Typography>
-              </div>
-            </div>
-            <Button
-             placeholder={undefined}
-              color="gray"
-              className="w-1/2 md:w-fit flex-shrink-0"
-              size="md"
-            >
-              follow
-            </Button>
-          </div>
-        </div>
-      </div>
-    </section>
+      </form>
+    </Card>
+    </div>
     </>
     );
   }
